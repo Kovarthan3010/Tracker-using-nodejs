7dd269373c67e57bf43af23835e3de1988c2798d
Kovarthan3010 <skovarthan7@gmail.com>
2022-06-28 15:11:18 +0530

    file write part working

--- Frontend/main.js
+++ Frontend/main.js
@@ -2,8 +2,11 @@ var express = require('express');
 const path = require('path');
 var app = express();
 var fs=require('fs');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 app.listen(8081);
+app.use(express.urlencoded({
+    extended : true 
+}));
 app.get("/",function(req,res)
 {
     
@@ -20,6 +23,16 @@ app.get("/remove.html",function(req,res)
 
 app.post("/add.html",function(req,res)
 {
-  var input=req.body.input;
-  console.log(input);
+  var input=req.body.postdetail;
+  fs.writeFile('txtfile.txt',input,function(err , data)
+  {
+    if(err)
+    {
+        res.send(err);
+    }
+    else
+    {
+        res.redirect('/');
+    }
+  })
 })
